a4711b23ff623012fb1b730984272890074e87d4
Attent1 <rafaeldsc22@outlook.com>
2024-05-09 22:04:59 -0300

    criado endpoint de login

--- src/main/java/br/com/fiap/SmartCash/Usuario/UsuarioController.java
+++ src/main/java/br/com/fiap/SmartCash/Usuario/UsuarioController.java
@@ -20,11 +20,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import io.swagger.v3.oas.annotations.Operation;  
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -37,62 +38,73 @@ public class UsuarioController {
 
     @Autowired
     UsuarioRepository repository;
-        
+
     @PostMapping
     @ResponseStatus(CREATED)
     @CacheEvict(allEntries = true)
-    @Operation(summary = "Cria um novo usuário.", description = "Cria um novo usuário no sistema.")  
-    public Usuario create(@RequestBody @Valid Usuario usuario){        
-        usuario.setLOGIN_USUARIO(geraLoginUsuario(usuario));                                         
+    @Operation(summary = "Cria um novo usuário.", description = "Cria um novo usuário no sistema.")
+    public Usuario create(@RequestBody @Valid Usuario usuario) {
+        usuario.setLOGIN_USUARIO(geraLoginUsuario(usuario));
         return repository.save(usuario);
     }
-    
+
     @GetMapping
     @Cacheable
-    @Operation(summary = "Lista todos os usuários.", description = "Retorna uma lista de todos os usuários no sistema.")  // Adicionado
-    public List<Usuario> readAll(){
+    @Operation(summary = "Lista todos os usuários.", description = "Retorna uma lista de todos os usuários no sistema.") // Adicionado
+    public List<Usuario> readAll() {
         return repository.findAll();
     }
 
     @GetMapping("{id}")
-    @Operation(summary = "Retorna um usuário por ID.", description = "Retorna um usuário pelo seu ID.")  
-    public ResponseEntity<Usuario> readItem(@PathVariable Long id){
+    @Operation(summary = "Retorna um usuário por ID.", description = "Retorna um usuário pelo seu ID.")
+    public ResponseEntity<Usuario> readItem(@PathVariable Long id) {
         return repository.findById(id)
-                         .map(ResponseEntity::ok) 
-                         .orElse(ResponseEntity.notFound().build());     
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("{id}")
     @ResponseStatus(OK)
     @CacheEvict(allEntries = true)
-    @Operation(summary = "Atualiza um usuário.", description = "Atualiza um usuário pelo seu ID.")  
-    public Usuario update(@PathVariable Long id, @RequestBody Usuario usuario){
+    @Operation(summary = "Atualiza um usuário.", description = "Atualiza um usuário pelo seu ID.")
+    public Usuario update(@PathVariable Long id, @RequestBody Usuario usuario) {
         verificarSeExisteUsuario(id);
         usuario.setID_USUARIO(id);
-        usuario.setLOGIN_USUARIO(geraLoginUsuario(usuario));   
+        usuario.setLOGIN_USUARIO(geraLoginUsuario(usuario));
         return repository.save(usuario);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(NO_CONTENT)
-    @Operation(summary = "Deleta um usuário.", description = "Remove um usuário pelo seu ID.")  
-    public void delete(@PathVariable Long id){        
+    @Operation(summary = "Deleta um usuário.", description = "Remove um usuário pelo seu ID.")
+    public void delete(@PathVariable Long id) {
         verificarSeExisteUsuario(id);
         repository.deleteById(id);
     }
 
     private Usuario verificarSeExisteUsuario(Long id) {
         return repository.findById(id)
-                  .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Usuário não encontrado"));
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Usuário não encontrado"));
     }
 
     private String geraLoginUsuario(Usuario usuario) {
         String documentoNumerico = usuario.getDOCUMENTO().replaceAll("[^0-9]", "");
-        String loginUsuario = usuario.getNOME().substring(0, 1) + 
-        (documentoNumerico.length() == 11 ? 
-        documentoNumerico.substring(7) 
-        : 
-        documentoNumerico.substring(10));
+        String loginUsuario = usuario.getNOME().substring(0, 1) +
+                (documentoNumerico.length() == 11 ? documentoNumerico.substring(7)
+                        : documentoNumerico.substring(10));
         return loginUsuario;
     }
+
+    @GetMapping("/login")
+    public Boolean login(@RequestParam String loginUsuario, @RequestParam String senhaUsuario) {
+        Usuario usuario = repository.LoginUsuario(loginUsuario);
+
+        if (usuario != null) {
+            if (usuario.getSENHA().equals(senhaUsuario)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/br/com/fiap/SmartCash/Usuario/UsuarioRepository.java
+++ src/main/java/br/com/fiap/SmartCash/Usuario/UsuarioRepository.java
@@ -1,7 +1,11 @@
 package br.com.fiap.SmartCash.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
+    @Query(value = "SELECT * FROM TB_USUARIO A WHERE A.LOGIN_USUARIO = :loginUsuario", nativeQuery = true)
+    Usuario LoginUsuario(@Param("loginUsuario") String loginUsuario);
 }
